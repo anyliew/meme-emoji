5acf6f72575b01e0798198f1491585291ff1e762
Anyliew <tikin.liew@outlook.com>
2026-03-06 21:39:09 +0800

    sync 菲比

--- src/memes/kurogames_phoebe_say.rs
+++ src/memes/kurogames_phoebe_say.rs
@@ -1,8 +1,10 @@
+use rand::RngExt;
 use skia_safe::{Color, IRect, textlayout::TextAlign};
 
 use meme_generator_core::error::Error;
+use meme_generator_core::meme::MemeOption;
 use meme_generator_utils::{
-    builder::InputImage,
+    builder::{InputImage, MemeOptions},
     canvas::CanvasExt,
     encoder::encode_png,
     image::ImageExt,
@@ -10,25 +12,63 @@ use meme_generator_utils::{
     tools::{load_image, local_date, new_paint},
 };
 
-use crate::{options::NoOptions, register_meme, tags::MemeTags};
+use crate::register_meme;
 
-const DEFAULT_TEXT: &str = "嘟嘟嘟说什么呢";
+#[derive(Clone, Debug, Default, serde::Deserialize)]
+pub struct Number {
+    pub number: Option<i32>,
+}
+
+impl MemeOptions for Number {
+    fn to_options(&self) -> Vec<MemeOption> {
+        vec![]
+    }
+}
+
+const DEFAULT_TEXT: &str = "菲比啾比";
+
 fn kurogames_phoebe_say(
     _: Vec<InputImage>,
     texts: Vec<String>,
-    _: NoOptions,
+    options: Number,
 ) -> Result<Vec<u8>, Error> {
     let text = if !texts.is_empty() { &texts[0] } else { DEFAULT_TEXT };
-    let frame = load_image("kurogames_phoebe_say/0.jpg")?;
+
+    let num = match options.number {
+        Some(n) => n,
+        None => rand::rng().random_range(1..=13),
+    };
+
+    let frame = load_image(&format!("kurogames_phoebe_say/{}.png", num))?;
+
+    let rects = [
+        IRect::from_ltrb(15, 29, 954, 387),
+        IRect::from_ltrb(0, 0, 515, 210),
+        IRect::from_ltrb(0, 0, 667, 206),
+        IRect::from_ltrb(0, 0, 512, 114),
+        IRect::from_ltrb(0, 0, 727, 249),
+        IRect::from_ltrb(0, 0, 800, 171),
+        IRect::from_ltrb(0, 0, 800, 250),
+        IRect::from_ltrb(0, 0, 1024, 222),
+        IRect::from_ltrb(0, 0, 800, 162),
+        IRect::from_ltrb(0, 0, 594, 254),
+        IRect::from_ltrb(0, 0, 1024, 217),
+        IRect::from_ltrb(0, 0, 361, 147),
+        IRect::from_ltrb(0, 0, 626, 149),
+    ];
+
+    let index = (num - 1) as usize;
+    let rect = rects[index];
+
     let mut surface = frame.to_surface();
     let canvas = surface.canvas();
     canvas.draw_text_area_auto_font_size(
-        IRect::from_ltrb(1, 1, 177, 86),
+        rect,
         text,
         10.0,
-        120.0,
+        200.0,
         text_params!(
-            font_families = &["FZKaTong-M19S"],
+            font_families = &["FZShaoEr-M11S"],
             text_align = TextAlign::Center,
             paint = new_paint(Color::from_rgb(0, 0, 0)),
         ),
@@ -40,11 +80,12 @@ fn kurogames_phoebe_say(
 register_meme!(
     "kurogames_phoebe_say",
     kurogames_phoebe_say,
-    min_texts = 0,
+    min_images = 0,
+    max_images = 0,
+    min_texts = 1,
     max_texts = 1,
-    tags = MemeTags::phoebe(),
     default_texts = &[DEFAULT_TEXT],
     keywords = &["菲比说"],
-    date_created = local_date(2025, 10, 6),
-    date_modified = local_date(2025, 10, 6),
+    date_created = local_date(2025, 5, 10),
+    date_modified = local_date(2026, 2, 16),
 );
